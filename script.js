532e2a6940b7005380340459070946f3fac0a4cd
Gizmal <gizmals@gmail.com>
2022-10-15 17:12:08 +0200

    Ajout écran des scores

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const cases = document.querySelectorAll('.case');
 const indicateur = document.getElementById('indicateur');
 const finDePartie = document.getElementById('finDePartie');
+const body = document.querySelector("body");
+const voile = document.createElement("div");
+voile.setAttribute("id", "voile");
 const joueur1 = 'X';
 const joueur2 = 'O';
+let scoreJ1 = 0;
+let scoreJ2 = 0;
+let matchNul = 0;
+let nbreCoup = 0;
 let tourJoueur = joueur1;
 
 const combinaisonsGagnantes = [
@@ -20,10 +27,21 @@ cases.forEach(maCase => {
     maCase.addEventListener('click', () => {
         if(maCase.innerText==="") {
             maCase.innerText=tourJoueur;
-            if(controleVictoire(tourJoueur)===true) {
-                console.log(`Victoire du joueur ${tourJoueur}`);
-            };
-            if(tourJoueur === joueur1) {
+            nbreCoup++;
+            if(controleVictoire(tourJoueur)) {
+                if(tourJoueur === joueur1) {
+                    scoreJ1++;
+                }
+                else {
+                    scoreJ2++;
+                }
+                ecranScore();
+            };            
+            if(nbreCoup === 9) {
+                matchNul++;                
+                ecranScore();
+            }
+            if( tourJoueur === joueur1 ) {
                 tourJoueur = joueur2;                
             }
             else { tourJoueur = joueur1;
@@ -37,8 +55,31 @@ function controleVictoire(tourJoueur) {
         if( cases[recupCombinaison[0]].innerText === tourJoueur &&
             cases[recupCombinaison[1]].innerText === tourJoueur &&
             cases[recupCombinaison[2]].innerText === tourJoueur) {
-        return true
+        return true;
         }                                    
-    }
-    return false            
+    }           
 }
+
+function ecranScore() {
+    const popup = document.createElement("div");
+    nbreCoup=0;
+    popup.classList.add("popup");
+    popup.innerHTML =   `
+                        <div class="popup-header">
+                            <p>Joueur 1  : ${scoreJ1}</p>
+                            <p>Joueur 2  : ${scoreJ2}</p>
+                            <p>Match Nul : ${matchNul}</p>                            
+                            <button id="rejouer">Rejouer</button>
+                        </div>
+                        ` 
+    body.append(voile);
+    body.append(popup);
+    boutonRejouer = document.getElementById("rejouer");
+    boutonRejouer.addEventListener("click", () => {
+        voile.remove();
+        popup.remove();
+        cases.forEach(maCase => {
+            maCase.innerText = "";
+        });
+    });
+};
